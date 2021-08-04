9fcd8ab8f38d73ac5090f43e498ac8810f9f0d55
Caleb Adewole <caleberioluwa@gmail.com>
2021-08-04 12:05:44 +0100

    add ncurses example 1

---
+++ examples/1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "ncurses.h"
+
+int main()
+{
+    initscr();
+    addch("H");
+    addch("E");
+    addch("L");
+    addch("L");
+    addch("0");
+    addch("!");
+    getch();
+    endwin();
+}
